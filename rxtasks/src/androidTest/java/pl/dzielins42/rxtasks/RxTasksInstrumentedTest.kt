c11df3e73b75dea1fadbcedfc46a5dff543301c6
dzielins42 <dzielins42@gmail.com>
2020-02-10 21:39:38 +0100

    Add tests

---
+++ rxtasks/src/androidTest/java/pl/dzielins42/rxtasks/RxTasksInstrumentedTest.kt
@@ -0,0 +1,93 @@
+package pl.dzielins42.rxtasks
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.CancellationException
+
+@RunWith(AndroidJUnit4::class)
+class RxTasksInstrumentedTest {
+
+    @Test
+    fun whenTasksReturnsValueSingleEmitsIt() {
+        // Arrange
+        val expectedValue = 42
+        val task: Task<Int> = Tasks.call {
+            Thread.sleep(1000)
+            expectedValue
+        }
+
+        // Act
+        val testObserver = task.asSingle().test().await()
+
+        // Assert
+        testObserver
+            .assertComplete()
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValue(expectedValue)
+    }
+
+    @Test
+    fun whenTasksThrowsExceptionSingleCallsOnError() {
+        // Arrange
+        val expectedError = Exception()
+        val task: Task<Int> = Tasks.call {
+            Thread.sleep(1000)
+            throw expectedError
+        }
+
+        // Act
+        val testObserver = task.asSingle().test().await()
+
+        // Assert
+        testObserver
+            .assertError(expectedError)
+    }
+
+    @Test
+    fun whenTasksHasCompletedWithValueSingleEmitsIt() {
+        // Arrange
+        val expectedValue = 42
+        val task: Task<Int> = Tasks.forResult(expectedValue)
+
+        // Act
+        val testObserver = task.asSingle().test().await()
+
+        // Assert
+        testObserver
+            .assertComplete()
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValue(expectedValue)
+    }
+
+    @Test
+    fun whenTasksHasCompletedWithErrorSingleCallsOnError() {
+        // Arrange
+        val expectedError = Exception()
+        val task: Task<Int> = Tasks.forException(expectedError)
+
+        // Act
+        val testObserver = task.asSingle().test().await()
+
+        // Assert
+        testObserver
+            .assertError(expectedError)
+    }
+
+    @Test
+    fun whenTasksWasCancelledSingleCallsOnErrorWithCancellationException() {
+        // Arrange
+        val task: Task<Int> = Tasks.forCanceled()
+
+        // Act
+        val testObserver = task.asSingle().test().await()
+
+        // Assert
+        testObserver
+            .assertError { error -> error is CancellationException }
+    }
+}
