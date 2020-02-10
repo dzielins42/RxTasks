a8b5520e3dc79a27a23265823206cac39180a54e
dzielins42 <dzielins42@gmail.com>
2020-02-10 21:42:58 +0100

    Add asSingle and some documentation

--- rxtasks/src/main/java/pl/dzielins42/rxtasks/RxTasks.kt
+++ rxtasks/src/main/java/pl/dzielins42/rxtasks/RxTasks.kt
@@ -1,10 +1,14 @@
 package pl.dzielins42.rxtasks
 
 import com.google.android.gms.tasks.Task
+import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.SingleEmitter
 import java.util.concurrent.CancellationException
 
+/**
+ * Wraps [Task] in [Single], which emits its value or error.
+ */
 fun <T : Any> Task<T>.asSingle(): Single<T> {
     return Single.create<T> { emitter ->
         if (isComplete || isCanceled) {
@@ -14,6 +18,14 @@ fun <T : Any> Task<T>.asSingle(): Single<T> {
             addOnCanceledListener { emitter.onError(CancellationException("Task cancelled")) }
         }
     }
+}
+
+/**
+ * Wraps [Task], which does not return a value, in [Completable], which
+ * completes when [Task] completes or calls `onError`.
+ */
+fun Task<Void>.aCompletable(): Completable {
+    return this.asSingle().ignoreElement()
 }
 
 private fun <T : Any> Task<T>.handleSingleEmitter(emitter: SingleEmitter<T>) {
